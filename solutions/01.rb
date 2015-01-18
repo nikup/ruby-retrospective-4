5763db4521560f30a2fa66933233248a33fefe81
nikup <nikupbg@gmail.com>
2015-01-18 17:52:06 +0200

    [Task1] Rename general function; Get rid of hash in favour of switch

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,24 +1,15 @@
 def series(name, number)
-  if name == 'summed'
-    return series('fibonacci', number) + series('lucas', number)
+  case name
+    when 'fibonacci' then generic_series([1, 1], number)
+    when 'lucas' then generic_series([2, 1], number)
+    when 'summed' then series('fibonacci', number) + series('lucas', number)
   end
-
-  series_at_position(series_start(name), number)
 end
 
-def series_at_position(previous, number)
+def generic_series(previous, number)
   if number == 1
     previous[0]
   else
-    series_at_position([previous[1], previous[0] + previous[1]], number - 1)
+    generic_series([previous[1], previous[0] + previous[1]], number - 1)
   end
-end
-
-def series_start(name)
-  start_at = {
-    fibonacci:  [1, 1],
-    lucas:      [2, 1],
-  }
-
-  start_at[name.to_sym]
 end
